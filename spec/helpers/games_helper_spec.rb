7bd6c90041e535958892e3dcdc5f80534acb8d11
Christophe Lucas <clucas4r@gmail.com>
2021-03-29 17:09:21 -0700

    change font

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GamesHelper, type: :helper do
   describe "#card_cell" do
     context 'positive' do
       it 'renders the background color' do
-         expect(helper.card_cell(true, 'content')).to eq "<td bgcolor=\"green\">content</td>"
+         expect(helper.card_cell(true, 'content')).to eq "<td bgcolor=\"green\" style=\"color:white\">content</td>"
       end
     end
 
