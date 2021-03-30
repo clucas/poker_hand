afa2608ca35d2c193ed55d3834b3cd44fc6aea59
Christophe Lucas <clucas4r@gmail.com>
2021-03-29 17:09:09 -0700

    change font

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,5 +1,5 @@
 module GamesHelper
   def card_cell(positive, content)
-    positive ? tag.td(content, { bgcolor: 'green'}) : tag.td(content)
+    positive ? tag.td(content, { bgcolor: 'green', style: 'color:white' }) : tag.td(content)
   end
 end
